e7371b71a83f8e9eb8c2f886add13770a69e8b2d
KushP12 <109982522+KushP12@users.noreply.github.com>
2022-08-10 10:15:48 -0400

    Update Tungsten Comparison.py

--- Tungsten Comparison.py
+++ Tungsten Comparison.py
@@ -1,4 +1,6 @@
 #Kramer's Law
+
+#importing the various libraries that are required
 import matplotlib.pyplot as plt
 import numpy as np
 from math import e
@@ -7,7 +9,7 @@ import xlrd
 from sklearn import preprocessing
 import statistics
 
-
+#created a function for the filtration, with variables that you can change 
 def Filtration(MolAtt, PathLen, Conc):
     absorb = MolAtt * PathLen * Conc
     return absorb
@@ -16,31 +18,30 @@ def Filtration(MolAtt, PathLen, Conc):
 
 loc = ('X-Ray data.xls')
 
-wb = xlrd.open_workbook(loc)     
-sheet = wb.sheet_by_index(0)
+wb = xlrd.open_workbook(loc) #allows access to the excel file to retrieve data     
+sheet = wb.sheet_by_index(0) #retrieves data line by line from excel
 
 df = pd.read_excel(r'X-Ray data.xls')
 
-def IntensityCal(Z, kVmax, I, Ka, Kb):
-    loc = ('X-Ray image dataT.xls')
+def IntensityCal(Z, kVmax, I, Ka, Kb): #defined a function to run the comparison
+    loc = ('X-Ray image dataT.xls') #retrieves data for Tungsten element
     wb = xlrd.open_workbook(loc)     
     sheet = wb.sheet_by_index(0)
     xarray2 = []
     yarray2 = []
     yarray2Area = []
-    for each in range(183):
+    for each in range(183): #append x-values to an array to be plotted as it can't be straight from the excel file
         xarray2.append(sheet.cell_value(each,0))
-    for each in range(183):
-        if (kVmax-20) <= sheet.cell_value(each, 0) <= (kVmax-10):
-    
+    for each in range(183): #append y-values to an array to be plotted as it can't be straight from the excel file
+        if (kVmax-20) <= sheet.cell_value(each, 0) <= (kVmax-10): #separates values that are to be normalized and not to be normalized
             yarray2Area.append(sheet.cell_value(each, 1))
             yarray2.append(sheet.cell_value(each, 1))
         else:
             yarray2.append(sheet.cell_value(each, 1))
-    area2 = np.trapz(np.array(yarray2Area), dx = 1)
+    area2 = np.trapz(np.array(yarray2Area), dx = 1) #Calculates the area under the values to be normalized. The area can be used to normalize the y-values so it easier to do a comparison
     yarray2New = []
     xarray2New = []
-    yarrayNorm2 = yarray2/(np.linalg.norm(yarray2))
+    yarrayNorm2 = yarray2/(np.linalg.norm(yarray2))# Function that normalizes all y-values
     #for each in xarray2:
      #   xarray2New.append(each*3)
         
@@ -51,30 +52,30 @@ def IntensityCal(Z, kVmax, I, Ka, Kb):
     plt.figure(1)
     plt.xlabel("Energy(keV)")
     plt.ylabel("Intensity")
-    plt.plot(xarray2,yarray2)
+    plt.plot(xarray2,yarray2) #Plots out the x-values and y-values
 
 
-    xarray = []
-    yarray = []
+    xarray = [] #X-values
+    yarray = [] #Y-values
     yarrayArea = []
     indexArr = []
     for i in range(1, kVmax):
         xarray.append(i)
     for i in range(1,kVmax):
-        if (Ka-1) < i < (Ka+1):
+        if (Ka-1) < i < (Ka+1): #identifies the points where the Ka characteristic ray occurs and appends to the yarray
             CR = ((i*10**3) - (Ka*10**3))**1.5
             yarray.append(((((CR+((kVmax - i)*Z)))*((e**((-22*10**2)/(i**3)))))))
-        if (Kb-1) < i < (Kb+1):
+        if (Kb-1) < i < (Kb+1): #identifies the points where the Kb characteristic ray occurs and appends to the yarray
             CR = ((i*10**3) - (Kb*10**3))**1.5
             yarray.append(((((CR+((kVmax - i)*Z)))*(((e**((-22*10**2)/(i**3))))))))
         else: 
-            if (kVmax-20) < i < (kVmax-10):
+            if (kVmax-20) < i < (kVmax-10): #appends the rest of the Y-values to the yarray to be plotted
                 yarrayArea.append(((((kVmax - i)*Z))*(((e**((-22*10**2)/(i**3)))))))
                 yarray.append(((((kVmax - i)*Z))*(((e**((-22*10**2)/(i**3)))))))
                 indexArr.append(xarray.index(i))
             else:
                 yarray.append(((((kVmax - i)*Z))*(((e**((-22*10**2)/(i**3)))))))
-    for each in yarray: 
+    for each in yarray: # this for statement removes the complex numbers from the y-array.
         if isinstance(each, complex):
             indexVal = yarray.index(each)
             yarray.remove(each)
@@ -84,24 +85,25 @@ def IntensityCal(Z, kVmax, I, Ka, Kb):
     #xarray.pop()
     
     
-    yarrayNorm = yarray/(np.linalg.norm(yarray))
+    yarrayNorm = yarray/(np.linalg.norm(yarray))#normalizes all the y-array values for the simulated data
     yarrayAreaNorm = []
     for each in indexArr:
         yarrayAreaNorm.append(yarrayNorm[each])
 
-
-    diffArray = np.divide(yarray2Area, yarrayAreaNorm)
+    #this to help the scaling of the data to make it easier to compare
+    diffArray = np.divide(yarray2Area, yarrayAreaNorm) 
     meanVal = statistics.mean(diffArray)
     #for each in indexArr:
      #     yarrayNorm[each] = yarrayNorm[each] * meanVal
     yarrayNorm = yarrayNorm * meanVal
-           
+     
+    #plots out the simulated data   
     plt.plot(xarray, yarrayNorm)
     plt.show()    
     return xarray, yarray
 
 
-
+#following code makes it more intuitive and user friendly by letting them choose the Max kVp and anode material
 userkVp = int(input("Enter max kVp: "))
 userMaterial = input("Enter which material you would like: ")
 for each in range(6):
