ba47418435538748f6d2fb16b4bd0061fb545763
KushP12 <109982522+KushP12@users.noreply.github.com>
2022-08-10 09:30:42 -0400

    Create Silver Comparison.py

---
+++ Silver Comparison.py
@@ -0,0 +1,118 @@
+#Kramer's Law
+import matplotlib.pyplot as plt
+import numpy as np
+from math import e
+import pandas as pd 
+import xlrd
+from sklearn import preprocessing
+import statistics
+
+
+def Filtration(MolAtt, PathLen, Conc):
+    absorb = MolAtt * PathLen * Conc
+    return absorb
+
+
+
+loc = ('X-Ray data.xls')
+
+wb = xlrd.open_workbook(loc)     
+sheet = wb.sheet_by_index(0)
+
+df = pd.read_excel(r'X-Ray data.xls')
+
+def IntensityCal(Z, kVmax, I, Ka, Kb):
+    loc = ('X-Ray image dataS.xls')
+    wb = xlrd.open_workbook(loc)     
+    sheet = wb.sheet_by_index(0)
+    xarray2 = []
+    yarray2 = []
+    yarray2Area = []
+    for each in range(183):
+        xarray2.append(sheet.cell_value(each,0))
+    for each in range(183):
+        if (kVmax-10) <= sheet.cell_value(each, 0) <= (kVmax-5):
+    
+            yarray2Area.append(sheet.cell_value(each, 1))
+            yarray2.append(sheet.cell_value(each, 1))
+        else:
+            yarray2.append(sheet.cell_value(each, 1))
+    area2 = np.trapz(np.array(yarray2Area), dx = 1)
+    yarray2New = []
+    xarray2New = []
+    yarrayNorm2 = yarray2/(np.linalg.norm(yarray2))
+    for each in yarrayNorm2:
+        yarray2New.append(each*4)
+    for each in xarray2:
+        xarray2New.append(each)
+        
+
+    #for each in yarray2: 
+     #   yarray2New.append(each/50)
+        
+    plt.figure(1)
+    plt.xlabel("Energy(keV)")
+    plt.ylabel("Intensity")
+    plt.plot(xarray2New,yarray2New)
+
+
+    xarray = []
+    yarray = []
+    yarrayArea = []
+    indexArr = []
+    for i in range(1, kVmax):
+        xarray.append(i)
+    for i in range(1,kVmax):
+        if (Ka-1) < i < (Ka+1):
+            CR = ((i*10**3) - (Ka*10**3))**1.5
+            yarray.append(((((CR+((kVmax - i)*Z)))*((e**((-22*10**2)/(i**3)))))))
+        if (Kb-1) < i < (Kb+1):
+            CR = ((i*10**3) - (Kb*10**3))**1.5
+            yarray.append(((((CR+((kVmax - i)*Z)))*(((e**((-22*10**2)/(i**3))))))))
+        else: 
+            if (kVmax-10) < i < (kVmax-5):
+                yarrayArea.append(((((kVmax - i)*Z))*(((e**((-22*10**2)/(i**3)))))))
+                yarray.append(((((kVmax - i)*Z))*(((e**((-22*10**2)/(i**3)))))))
+                indexArr.append(xarray.index(i))
+            else:
+                yarray.append(((((kVmax - i)*Z))*(((e**((-22*10**2)/(i**3)))))))
+    for each in yarray: 
+        if isinstance(each, complex):
+            indexVal = yarray.index(each)
+            yarray.remove(each)
+    
+        
+    #xarray.pop()
+    #xarray.pop()
+    
+    
+    yarrayNorm = yarray/(np.linalg.norm(yarray))
+    yarrayAreaNorm = []
+    for each in indexArr:
+        yarrayAreaNorm.append(yarrayNorm[each])
+    yarrayNew2 = []
+    #for each in yarray:
+     #   yarrayNew2.append(each/2)
+    for each in yarray:
+        yarrayNew2.append(each*50)
+        
+
+
+    #diffArray = np.divide(yarray2Area, yarrayAreaNorm)
+    #meanVal = statistics.mean(diffArray)
+    #for each in indexArr:
+     #     yarrayNorm[each] = yarrayNorm[each] * meanVal
+    #yarrayNorm = yarrayNorm * meanVal
+           
+    plt.plot(xarray, yarrayNorm)
+    plt.show()    
+    return xarray, yarray
+
+
+
+userkVp = int(input("Enter max kVp: "))
+userMaterial = input("Enter which material you would like: ")
+for each in range(6):
+    if sheet.cell_value(each,0) == userMaterial:
+        IntensityCal(sheet.cell_value(each, 0 + 1), userkVp, (1*10**-3), sheet.cell_value(each, 2), sheet.cell_value(each, 3))
+    
